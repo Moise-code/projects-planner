4006087f2a6614568c02dfb7ff76e6fd595ae3c9
Moise-code <moisewowe@gmail.com>
2023-02-01 01:35:32 -0800

    targeting the list of projects in the sections

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,27 @@
-// create class for projects of one box
-class ProjectList{
-
+// create a tool tip class to display tool tip when click on info button
+class Tooltip {
+  
 }
 // create class for one single project item in the list.
 class ProjectItem{
 
 }
-// create a tool tip class to display tool tip when click on info button
-class Tooltip {
-  
+// create class for projects of one box
+class ProjectList{
+constructor(type){ 
+  // now we are going to get the list of projects on both sides
+  const projItems = document.querySelectorAll(`#${type}-projects li`);
+  console.log(projItems);
+
+}
 }
 /// create class to manage the whole application overall.
 class App{
+ static init(){
+
+  const activeProjectsList = new ProjectList('active');
+  const finishedProjectsList = new ProjectList('finished');
   
+  }
 }
+App.init();
