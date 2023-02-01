3694e3706d96976276152058c368cf3da354bc57
Moise-code <moisewowe@gmail.com>
2023-02-01 01:18:06 -0800

    adding the proper needed classes for this project

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+// create class for projects of one box
+class ProjectList{
+
+}
+// create class for one single project item in the list.
+class ProjectItem{
+
+}
+// create a tool tip class to display tool tip when click on info button
+class Tooltip {
+  
+}
+/// create class to manage the whole application overall.
+class App{
+  
+}
