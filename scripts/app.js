a7ba2e5d07f658615601f5381c835efe14f428c1
Moise-code <moisewowe@gmail.com>
2023-02-02 10:23:35 +0200

    adding the functionality to classes

--- scripts/app.js
+++ scripts/app.js
@@ -4,67 +4,36 @@ class Tooltip {
 }
 // create class for one single project item in the list.
 class ProjectItem{
-  constructor(id){
-    this.id = id
-    this.connectSwitchConnector();
-    this.connectMoreInfoButton();
-  }
- connectSwitchConnector(){
-  // we are now first targeting the projects element
-  const projectItemElement = document.querySelector(this.id);
-  const switchBtn = projectItemElement.querySelector('button:last-of-type');
-  switchBtn.addEventListerner('click',)
-   
- }
- connectMoreInfoButton(){
-
- }
+  // creating constuctor with id since it is the one one passed down there/.
+constructor(id){
+  this.id = id;
+}
 }
 // create class for projects of one box
 class ProjectList{
 // the initial array of project list in a particular box
 projects = []
+constructor(type){
+// now we are going to get the list of items in the particular box
+//which is in the end the three projects in general
+const projItems = document.querySelectorAll(`#${type}-projects li`)
+// we are going to use for each project in the list of project to get the id
+projItems.forEach((projItem) =>{
+  // we are goin to get the id but from the class projectItem by instatiating it.
+  const projId = new ProjectItem(projItem.id);
+  this.projects.push(projId);
+  console.log(projId);
+})
 
-constructor(type){ 
-  this.type = type;
-
-  // now we are going to get the list of projects on both sides
-  const projItems = document.querySelectorAll(`#${type}-projects li`);
- // now lets use for each to get ids in the projectItems
- projItems.forEach((projId) =>{
-   this.projects.push(new ProjectItem(projId.id));
- })
- console.log(this.projects)
-
-}
-
-// create a switch handler method
-setSwitchHandlerFunction(switchHandlerFunction){
-  this.switchHandler = switchHandlerFunction;
-}
-
-// create method to add movie from the array of removed ones.
-
-addProject(){
 
 }
 
-// create a  method to switch project away from array list
-switchProject(projectId){
-  // create a new variable to contain the spliced element in array
-  // const projectIndex = this.projects.findIndex(p => p.id === projectId)
-  // this.projects.splice(projectIndex, 1);
-// another alternative is to use filter and keeps the valuated items
-this.projects.filter(p => p.id !== projectId);
-}
 }
 /// create class to manage the whole application overall.
 class App{
- static init(){
-
-  const activeProjectsList = new ProjectList('active');
-  const finishedProjectsList = new ProjectList('finished');
-  
-  }
+static init(){
+const activeProjectList = new ProjectList('active')
+const finishedProjectList = new ProjectList('finished')
+}
 }
 App.init();
